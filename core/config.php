432fa775319f58192ed9e70db6894072cd4b9cfe
aidaabdelazez5-cyber <aidaabdelazez5@gmail.com>
2026-07-04 00:40:48 +0300

    changes

--- core/config.php
+++ core/config.php
@@ -1,10 +1,11 @@
 <?php
-    return [
-        'database' => [
-        'host' =>'localhost',
-        'user' =>'root',
-        'pass' =>'',
-        'port'  => 3306,
-        ],
-        ];
+$config = [
+    'host' => '125.0.0.1',       
+    'port' => '3306',       
+    'dbname' => 'Task_2',   
+    'charset' => 'utf8mb4'       
+];
+
+$db = new Database($config, 'root', 'aida');
+
         
